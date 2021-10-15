ffb390a120fb7eb18efa00193868739f6fc48eeb
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-10-15 15:01:35 +0200

    fix: rename test aswell

--- src/test/java/com/insession/securityproject/api/services/AuthPinCodeServiceTest.java
+++ src/test/java/com/insession/securityproject/api/services/AuthPinCodeServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AuthTokenServiceTest {
+class AuthPinCodeServiceTest {
     private static AuthPinCodeService authTokenService;
 
     @BeforeEach
